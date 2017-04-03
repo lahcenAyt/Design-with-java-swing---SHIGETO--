2d142bac5a791f65d01cf89704ff86e7d779ff7c
lahcenAyt <lahcen.ayt.1988@gmail.com>
2017-04-03 16:00:19 +0200

    Java swing

---
+++ AppDesktop/src/com/MyPainter.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import javax.swing.JProgressBar;
+import javax.swing.Painter;
+
+class MyPainter implements Painter<JProgressBar> {
+
+    private final Color color;
+
+    public MyPainter(Color c1) {
+        this.color = c1;
+    }
+    @Override
+    public void paint(Graphics2D gd, JProgressBar t, int width, int height) {
+        gd.setColor(color);
+        gd.fillRect(0, 0, width, height);
+    }
+}
